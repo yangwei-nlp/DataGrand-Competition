5adb3608195ff6710ca10d5316ca8e296cdd44b0
yangwei <yangwei02@purvar.com>
2020-01-29 11:36:13 +0800

    打印词典数目

--- 2018-competition/model.py
+++ 2018-competition/model.py
@@ -18,7 +18,7 @@ if os.path.exists(modelpath):
     corpus = train_set['word_seg'].swifter.apply(lambda line: line.split())
     tokenizer = T.Tokenizer(num_words=200000, lower=False, filters="")
     [tokenizer.fit_on_texts(sentence) for sentence in corpus]
-    print(tokenizer.index_word)
+    print(len(tokenizer.index_word))
 
     model = Word2Vec(corpus.values.tolist(), size=200, window=5, min_count=1,
                      iter=10, workers=cpu_count(), max_vocab_size=200000)
