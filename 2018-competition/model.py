da8da4fe681ca27daa668ba092f28b897e734409
yangwei <yangwei02@purvar.com>
2020-02-01 11:34:50 +0800

    服务器测试

--- 2018-competition/model.py
+++ 2018-competition/model.py
@@ -1,8 +1,8 @@
-from gensim.test.utils import common_texts, get_tmpfile
-import tensorflow.keras.preprocessing.text as T
+from tensorflow.keras.preprocessing import text
 from multiprocessing import cpu_count
 from gensim.models import Word2Vec
 from tensorflow import keras
+import tensorflow as tf
 import pandas as pd
 import numpy as np
 import swifter
@@ -18,14 +18,78 @@ modelpath = "word2vec.model"
 if not os.path.exists(modelpath):
     train_set = pd.read_csv('data/train_set.csv', index_col=0)
     corpus = train_set['word_seg'].swifter.apply(lambda line: line.split())
-    # tokenizer = T.Tokenizer(num_words=200000, lower=False, filters="")
-    # [tokenizer.fit_on_texts(sentence) for sentence in corpus]
-    # print(len(tokenizer.index_word))
 
-    model = Word2Vec(corpus.values.tolist(), size=200, window=5, min_count=1,
-                     iter=10, workers=cpu_count(), max_vocab_size=200000)
-    model.save(modelpath)
+    w2c_model = Word2Vec(corpus.values.tolist(), size=200, window=5, min_count=1,
+                         iter=10, workers=cpu_count(), max_vocab_size=200000)
+    w2c_model.save(modelpath)
 else:
-    Word2Vec.load(modelpath)
+    w2c_model = Word2Vec.load(modelpath)
 
-# model.wv.vectors
+embedding_matrix = w2c_model.wv.vectors
+
+
+# 2.构建模型
+def my_model(max_len, embedding_matrix):
+    content = keras.layers.Input(shape=(max_len,))
+    embedding = keras.layers.Embedding(input_dim=embedding_matrix.shape[0],
+                                       weights=[embedding_matrix],
+                                       output_dim=embedding_matrix.shape[1],
+                                       trainable=False)
+    x = keras.layers.SpatialDropout1D(0.1)(embedding(content))
+
+    x = keras.layers.Bidirectional(keras.layers.GRU(200, return_sequences=True))(x)
+    x = keras.layers.Bidirectional(keras.layers.GRU(200, return_sequences=True))(x)
+
+    avg_pool = keras.layers.GlobalAveragePooling1D()(x)
+    max_pool = keras.layers.GlobalMaxPooling1D()(x)
+
+    conc = tf.concat([avg_pool, max_pool], axis=-1)
+
+    x = keras.layers.Dropout(0.2)(
+        keras.layers.Activation(activation="relu")(keras.layers.BatchNormalization()(keras.layers.Dense(1000)(conc))))
+    x = keras.layers.Activation(activation="relu")(keras.layers.BatchNormalization()(keras.layers.Dense(500)(x)))
+    output = keras.layers.Dense(19, activation="softmax")(x)
+
+    model = keras.models.Model(inputs=content, outputs=output)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+model = my_model(512, embedding_matrix)
+
+# 3.构造数据集
+train_set = pd.read_csv('data/train_set.csv', index_col=0)
+test_set = pd.read_csv('data/test_set.csv', index_col=0)
+
+train_set
+
+tokenizer = text.Tokenizer(num_words=500000, lower=False, filters="")
+tokenizer.fit_on_texts(train_set['word_seg'].values.tolist() +
+                       test_set['word_seg'].values.tolist())
+
+# print(len(tokenizer.index_word))
+
+trains = keras.preprocessing.sequence.pad_sequences(
+    tokenizer.texts_to_sequences(train_set['word_seg'].values), maxlen=512
+)
+
+tests = keras.preprocessing.sequence.pad_sequences(
+    tokenizer.texts_to_sequences(test_set['word_seg'].values), maxlen=512
+)
+
+trainings = tf.data.Dataset.from_tensor_slices((trains,train_set['class']))
+# trainings = trainings.repeat(10)
+# trainings = trainings.batch(128)
+
+# for X, Y in trainings:
+#     print(X.shape)
+#     print(Y.shape)
+
+checkpoint_dir = "model/cp-{epoch:04d}.ckpt"
+callbacks = tf.keras.callbacks.ModelCheckpoint(checkpoint_dir,
+                                               verbose=1,
+                                               save_weights_only=True,
+                                               period=10)
+
+
+model.fit(trainings, batch_size=128, epochs=1, callbacks=callbacks)
